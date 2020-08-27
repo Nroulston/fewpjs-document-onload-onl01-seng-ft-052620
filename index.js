906233ebff00aa21cd454626d086f58bdde329c7
Nroulston <nicolas.roulston@gmail.com>
2020-08-27 12:59:05 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  let pElement = document.getElementById('text')
+  pElement.innerHTML = "This is really cool!"
+});
+
